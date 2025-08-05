61c571b20edafb747ba83a14f912352b367c058f
Ydiboras04 <nomenymitia.andria@gmail.com>
2025-08-05 18:32:47 +0300

    name changed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,10 +8,10 @@ function Home(){
                     <img src="/illusion.png" alt="logo" srcset="/illusion.png" />
                 </div>
                 <div className="title">
-                    <h1>Mirage Studio 2D</h1>
+                    <h1>Welcome , <br/> Mirage Studio</h1>
                 </div>
                 <div className="slogan">
-                    <p><q><i>Where illusion comes alive in 2D.</i></q></p>
+                    <p><q><i>Where illusion comes alive</i></q></p>
                 </div>
                 <div className="explore">
                     <p>EXPLORE OUR WORLD</p>
